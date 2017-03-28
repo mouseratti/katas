0a8b194da694e9077baa315fc118f7f99d3b7df6
Mikhail Koshel <mouseratti@gmail.com>
2017-03-29 00:07:24 +0600

    kata1 finished in golang

--- kata1/golang/kata.go
+++ kata1/golang/kata.go
@@ -1,5 +1,24 @@
 package kata
 
+import "strings"
+
 func MakeKata(inputted string) string {
-    return ""
+    lower := strings.ToLower(inputted)
+
+    outputted := ""
+    d := make(map[string]string)
+
+    for _, symbol := range lower {
+        symbol := string(symbol)
+        if d[symbol] == "" {
+            d[symbol] = "("
+        } else {
+            d[symbol] = ")"
+        }
+    }
+    for _, symbol := range lower {
+        symbol := string(symbol)
+        outputted += d[symbol]
+    }
+    return outputted
 }
--- kata1/golang/kata_test.go
+++ kata1/golang/kata_test.go
@@ -5,13 +5,16 @@ import "testing"
 var FIXTURES [][]string = [][]string{
     {"eeee", "))))"},
     {"eeeae", ")))()"},
+    {"The Goal of this", "))()()(())()))(("},
+    {"Where Each Character", "())))))))))))))))())"},
+    {"Exercise is to Convert", ")()))))))))))))))(()))"},
 }
 
 func TestKata1(t *testing.T) {
     for _, fixture := range FIXTURES {
         result := MakeKata(fixture[0])
         if result != fixture[1] {
-            t.Error(result, " is not equal to ", fixture[0])
+            t.Error(result, " is not equal to ", fixture[1])
         }
     }
 }
