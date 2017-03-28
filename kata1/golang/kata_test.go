96c192043f14a94ff805f84956479bbd98fd4738
Mikhail Koshel <mouseratti@gmail.com>
2017-03-28 23:44:17 +0600

    modified golang test for kata1

--- kata1/golang/kata_test.go
+++ kata1/golang/kata_test.go
@@ -2,20 +2,16 @@ package kata
 
 import "testing"
 
-var FIXTURES = [][]string{
-    {"eeee", "((((())"},
-    {"eeee", "((((())"},
+var FIXTURES [][]string = [][]string{
+    {"eeee", "))))"},
+    {"eeeae", ")))()"},
 }
 
 func TestKata1(t *testing.T) {
-
-    t.Error(FIXTURES)
-    // for _, fixture := range FIXTURES {
-
-    //     result := MakeKata(fixture[0])
-
-    //     if result != fixture[1] {
-    //         t.Error(result, " is not equal to ", fixture[0])
-    //     }
-    // }
+    for _, fixture := range FIXTURES {
+        result := MakeKata(fixture[0])
+        if result != fixture[1] {
+            t.Error(result, " is not equal to ", fixture[0])
+        }
+    }
 }
