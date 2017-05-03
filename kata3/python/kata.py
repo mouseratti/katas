302fe32046584a7d209bcc91adb4c3df01481464
Mikhail Koshel <mouseratti@gmail.com>
2017-05-03 16:03:46 +0600

    kata3 updated

--- kata3/python/kata.py
+++ kata3/python/kata.py
@@ -34,26 +34,37 @@
 #     return subnet, broadcast
 
 
+# def make_kata(ipaddr):
+#     subnet = broadcast = ''
+#     ip, mask = ipaddr.split("/")
+#     ip = ''.join([bin(int(x) + 256)[3:] for x in ip.split(".")])
+#     mask = int(mask)
+#     assert len(ip) == 32, "ip is %s; len(ip) is %s" % (ip, len(ip))
+#     subnet = ip[:mask] + '0' * (32 - mask)
+#     broadcast = ip[:mask] + '1' * (32 - mask)
+#     assert len(subnet) == len(broadcast) == 32
+#     def to_decimal_ip(binary_ip):
+#         iplist = [binary_ip[x:x+8] for x in range(0, len(binary_ip), 8)]
+#         for position, binary_octet in enumerate(iplist):
+#             octet = 0
+#             for pos, symbol in enumerate(binary_octet):
+#                 octet += int(symbol) * 2 ** (7 - pos)
+#             iplist[position] = str(octet)
+#         return '.'.join(iplist)
+#     return to_decimal_ip(subnet), to_decimal_ip(broadcast)
+
+
 def make_kata(ipaddr):
-    subnet = broadcast = ''
     ip, mask = ipaddr.split("/")
-    ip = ''.join([bin(int(x) + 256)[3:] for x in ip.split(".")])
     mask = int(mask)
-    assert len(ip) == 32, "ip is %s; len(ip) is %s" % (ip, len(ip))
+    ip = ''.join([bin(int(octet) + 256)[3:] for octet in ip.split('.')])
+    assert len(ip) == 32
     subnet = ip[:mask] + '0' * (32 - mask)
     broadcast = ip[:mask] + '1' * (32 - mask)
-    assert len(subnet) == len(broadcast) == 32
-    def to_decimal_ip(binary_ip):
-        iplist = [binary_ip[x:x+8] for x in range(0, len(binary_ip), 8)]
-        for position, binary_octet in enumerate(iplist):
-            octet = 0
-            for pos, symbol in enumerate(binary_octet):
-                octet += int(symbol) * 2 ** (7 - pos)
-            iplist[position] = str(octet)
+
+    def to_decimal_ip(ip):
+        iplist = [ip[x:x + 8] for x in range(0, 32, 8)]
+        for position, octet in enumerate(iplist):
+            iplist[position] = str(sum([int(sym) * 2 ** (7 - pos) for pos, sym in enumerate(octet)]))
         return '.'.join(iplist)
     return to_decimal_ip(subnet), to_decimal_ip(broadcast)
-
-
-
-
-
