9dc254186a8defaa96e02db5e6e87bac38db227f
Mikhail Koshel <mouseratti@gmail.com>
2017-11-15 10:27:56 +0600

    Updated kata6

--- src/katas/kata6/2.go
+++ src/katas/kata6/2.go
@@ -2,26 +2,23 @@ package kata6
 
 import "katas/fixtures"
 
-//TODO fix this kata
 func make_kata2(f *fixtures.Fixture) {
 	length := len(f.Input)
 	last := length - 1
-	current := last
+	current := last - 1
 	var storage int64
 	var position int
 
 	for current >= 0 {
-		for i := current - 1; i < length; i++ {
-			if f.Input[current] < f.Input[i] {
+		for i := last; i > current; i-- {
+			if f.Input[current] > f.Input[i] {
 				position = i
-			} else {
-				position = last
+				storage = f.Input[current]
+				for j := current; j < position; j++ {
+					f.Input[j] = f.Input[j+1]
+				}
+				f.Input[position] = storage
 			}
-			storage = f.Input[current]
-			for j := current; j < position; j++ {
-				f.Input[j] = f.Input[j+1]
-			}
-			f.Input[position] = storage
 		}
 		current-=1
 	}
--- src/katas/kata6/3_test.go
+++ src/katas/kata6/3_test.go
@@ -7,17 +7,17 @@ import (
 	"katas/fixtures"
 )
 
-func Test_make_kata2(t *testing.T) {
+func Test_make_kata3(t *testing.T) {
 	f := fixtures.GetFixturePointer(100)
 	fmt.Println(f.Input)
-	make_kata2(f)
+	make_kata3(f)
 	assert.True(t, f.Is_sorted(), fmt.Sprintf("%v is not sorted!", f.Input))
 	fmt.Println(f.Input)
 }
 
-func Benchmark_make_kata2(b *testing.B) {
+func Benchmark_make_kata3(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		f:= fixtures.GetFixturePointer(100)
-		make_kata2(f)
+		make_kata3(f)
 	}
 }
