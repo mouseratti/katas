b10421a37ca2ac3ab3eec897b870b52e0aaf1674
Mikhail Koshel <mouseratti@gmail.com>
2018-09-25 12:24:56 +0600

    added dirtree

---
+++ src/katas/coursera/dirtree/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "os"
+import "io"
+
+//"fmt"
+//"io"
+//import 	"os"
+
+func main() {
+	out := os.Stdout
+	if !(len(os.Args) == 2 || len(os.Args) == 3) {
+		panic("usage go run main.go . [-f]")
+	}
+	path := os.Args[1]
+	printFiles := len(os.Args) == 3 && os.Args[2] == "-f"
+	err := dirTree(out, path, printFiles)
+	if err != nil {
+		panic(err.Error())
+	}
+}
+
+func dirTree(out io.Reader, path string, doPrintFiles bool) error {
+	return nil
+}
